5b5f625614175d55e2c832541ad1979aa1eb733c
Joseph Wynn <joseph@wildlyinaccurate.com>
2016-07-29 22:15:32 +0100

    Tidy up the five slice layout

--- src/components/five-slice.js
+++ src/components/five-slice.js
@@ -5,19 +5,19 @@ class FiveSlice extends React.Component {
   render () {
     return (
       <div className="row c-slice">
-        <div className="col-s-1 col-m-1/3 col-l-1/4 col-xl-1/5">
+        <div className="col-s-1 col-m-1/3 col-l-1/4">
           <Promo {...this.props.items[0]} />
         </div>
 
-        <div className="col-s-1 col-m-1/3 col-l-1/4 col-xl-1/5">
+        <div className="col-s-1 col-m-1/3 col-l-1/4">
           <Promo {...this.props.items[1]} />
         </div>
 
-        <div className="col-s-1 col-m-1/3 col-l-1/4 col-xl-1/5">
+        <div className="col-s-1 col-m-1/3 col-l-1/4">
           <Promo {...this.props.items[2]} />
         </div>
 
-        <div className="col-l-1/4 col-xl-2/5">
+        <div className="col-l-1/4">
           <div className="row">
             <div className="col-s-1/2 col-l-1">
                 <Promo {...this.props.items[3]} includeImage={false} includeTimestamp={false} includeSection={false} />
